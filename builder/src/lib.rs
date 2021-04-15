1554b4e38df1e9b38c2efd293dec4af2513a30b2
TsuITOAR <itoar@outlook.com>
2021-04-16 00:15:46 +0800

    name_fix

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -6,15 +6,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let input: syn::DeriveInput = syn::parse(input).unwrap();
     //eprintln!("INPUT:\n{:#?}", input);
     let struct_name = input.ident;
-    let struct_builder_name = format_ident!("{}Builder", struct_name);
+    let builder_name = format_ident!("{}Builder", struct_name);
     let output = quote! {
         impl #struct_name{
-            fn builder()->#struct_builder_name{
-                #struct_builder_name::default()
+            fn builder()->#builder_name{
+                #builder_name::default()
             }
         }
         #[derive(Default)]
-        struct #struct_builder_name{
+        struct #builder_name{
             executable: Option<String>,
             args: Option<Vec<String>>,
             env: Option<Vec<String>>,
